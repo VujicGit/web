72fd1846d9e9dc98130716f37a83d3a3561f4949
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2020-08-12 00:16:01 +0200

    ApartmentDAO

---
+++ src/dao/impl/ApartmentDAO.java
@@ -0,0 +1,61 @@
+package dao.impl;
+
+import beans.Apartment;
+
+public class ApartmentDAO implements dao.cruddao.ApartmentDAO {
+
+	@Override
+	public int count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete(Apartment entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean existsById(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterable<Apartment> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Apartment findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Apartment entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saveAll(Iterable<Apartment> entities) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
