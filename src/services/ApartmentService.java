bd58201747c4ef2bf7d6c761c54e379298c8361b
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2020-08-23 13:24:08 +0200

    popcetna stranica

---
+++ src/services/ApartmentService.java
@@ -0,0 +1,47 @@
+package services;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import beans.Apartment;
+import dao.impl.ApartmentDAO;
+
+@Path("/apartments")
+public class ApartmentService {
+	
+	@Context
+	ServletContext ctx;
+	
+	public ApartmentService() {
+		System.out.println("dadada");
+	}
+	
+	@PostConstruct
+	public void init() {
+		if(ctx.getAttribute("apartmentDAO") == null) {
+			String contextPath = ctx.getRealPath("");
+	    	System.out.println(contextPath);
+			ctx.setAttribute("apartmentDAO", new ApartmentDAO(contextPath));
+		}
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Apartment> getAllApartments() {
+		ApartmentDAO dao = (ApartmentDAO) ctx.getAttribute("apartmentDAO");
+		return dao.findAll();
+	}
+	
+	
+}
